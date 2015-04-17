ce5fa64bcd437f5379dd22661873fe24b6b7d9da
Andy Chen <jiqiang.ch@gmail.com>
2015-04-17 21:53:48 +0800

    update to avoid the blank data; add the file information

--- insert.php
+++ insert.php
@@ -1,7 +1,16 @@
 <?php
+/*
+* File Name:insert.php
+* Content:insert the poem to database
+* Version:1.0.1
+* Modifier: Andy Chen
+*/
 include("connect.php");
 
 mysql_select_db("a0209115716", $con);
+
+//judgement the data is blank or not
+if (($_POST[First]!=0) and ($_POST[Second]!=0) and ($_POST[Third]!=0)){
 
 $sql="INSERT INTO threeline (First, Second, Third,Name)
 VALUES
@@ -12,15 +21,18 @@ if (!mysql_query($sql,$con))
   die('Error: ' . mysql_error());
   }
   
-$result = mysql_query("SELECT * FROM threeline WHERE Name='$_POST[Name]'"); 
-$row = mysql_fetch_array($result);
+	$result = mysql_query("SELECT * FROM threeline WHERE Name='$_POST[Name]'"); 
+	$row = mysql_fetch_array($result);
   
 
-  //echo "<tr>" . $row['ID'] . "</tr>";
+  	//echo "<tr>" . $row['ID'] . "</tr>";
 
-//echo "1 record added";
-mysql_close($con);
-$add = "show.php?ID=" . $row['ID'] ;   
+	//echo "1 record added";
+	mysql_close($con);
+	$add = "show.php?ID=" . $row['ID'] ;   
 	header("Location:" . $add); 
-	
+} else {
+	echo "<p style='text-align:center; width:100%' ><br>Dear,the data is blank, please submit again!<br> ";
+	echo "<a href='index.php' text-align='center' width='100%'>返回</a> ";
+}	
 ?>
