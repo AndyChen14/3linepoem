1059834eefea94e003144311d596c8335562da86
Andy Chen <jiqiang.ch@gmail.com>
2015-04-18 22:57:47 +0800

    update the config file of Mysql

--- connect.php
+++ connect.php
@@ -1,11 +1,14 @@
 
 <?php
-echo header("content-type:text/html;charset=utf-8");  
-$con = mysql_connect("localhost","username","password");
-if (!$con)
-  {
-  die('Could not connect: ' . mysql_error());
-  }
+echo header("content-type:text/html;charset=utf-8");
+
+$mysql_db_hostname = "localhost";
+$mysql_db_user = "a0209115716";
+$mysql_db_password = "49910565";
+$mysql_db_database = "a0209115716";
+
+$con = mysql_connect($mysql_db_hostname, $mysql_db_user, $mysql_db_password) or die("Could not connect database");
+
  //设置客户端和连接字符集  
     mysql_query("set names utf8");  
 
