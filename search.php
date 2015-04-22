c9b044749555b13167471c6432efb5267e6faa0f
Andy Chen <jiqiang.ch@gmail.com>
2015-04-22 14:16:26 +0800

    auto clear the input value

--- search.php
+++ search.php
@@ -11,6 +11,14 @@
 <link rel="stylesheet" type="text/css" href="css/style.css">
 <script language="javascript" type="text/javascript">
 <!--
+var state1=0;
+function clearcontent1(x)
+{
+  if (state1==0){
+  x.value ="";
+  state1=1;
+  }
+}
 function postChk(theForm){
   
   if(theForm.ID.value.replace(/^[\s\r\n]+|[\s\r\n]+$/,'')=='' ){
@@ -31,12 +39,12 @@ function postChk(theForm){
 </head>
 <body>
 
-<div id="head"><p>三行情诗</p></div>
+<div id="head"><p>三行情诗·清华</p></div>
 
 <div id="poem">
 <form action="show.php" method="get" name="searchform" id="searchform" onsubmit="return postChk(this)">
 	<br>
-	<p class="middle">ID: <input type="text" name="ID" value="请输入三行情诗ID" /></p>
+	<p class="middle">ID: <input type="text" name="ID" value="请输入三行情诗ID" onfocus="clearcontent1(ID)" /></p>
 	<br>
 	<p class="middle"> <input type="submit"  value="查阅"/></p>
 	<p class="middle">&nbsp &nbsp </p>
