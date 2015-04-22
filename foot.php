fa5d23edb04ef6b6417dd5130b4c8d235e560c30
Andy Chen <jiqiang.ch@gmail.com>
2015-04-22 14:25:20 +0800

    add some message about the author

--- foot.php
+++ foot.php
@@ -1,7 +1,7 @@
 <?php
 echo"<div id='foot'>";
 echo "<h6><a href='http://www.qiyar.com/threeline/allshow.php'>所有</a>|<a href='http://www.qiyar.com/threeline/index.php'>写一首</a>|<a href='http://www.qiyar.com/threeline/search.php'>查阅</a></h6>";
-echo "<h6>Designed by THU_DMN and CJQ.</h6>";
+echo '<h6>Designed by THU and <a href="qiyar.html">QIYAR</a>.</h6>';
 echo "</div>";
 
 ?>
